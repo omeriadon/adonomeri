5df8b7a25bfc85a8d81d3e3acbfda6fe24afe2ce
Adon O <adon.omeri@student.education.wa.edu.au>
2025-03-06 21:40:39 +0800

    fck v4 ill stick to v3

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,6 @@
 module.exports = {
   plugins: {
-    '@tailwindcss/postcss': {
-      // Enable experimental features for Tailwind CSS v4
-      experimental: {
-        // Color opacity utilities
-        colorOpacityUtilities: true,
-        // All modern utilities
-        modernUtilities: true,
-      }
-    },
+    tailwindcss: {},
     autoprefixer: {},
   },
 }
